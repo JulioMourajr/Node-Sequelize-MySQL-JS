2dc03c4e6f08005c50078f05217d7406c5c04e2d
JulioMourajr <juliomourajr@hotmail.com>
2023-05-30 15:34:12 -0300

    CRUD da tabela offices terminado

--- src/app.js
+++ src/app.js
@@ -4,9 +4,15 @@ require('./config/db');
 
 const app = express();
 
+const router = require('./routes');
+
 app.get('/', (req, res) => {
   res.send({message:'Bem vindo a nossa API!'});
 })
+
+app.use(express.json());
+
+app.use(router);
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+const officesRouter = require('./officesRoute');
+
+router.use('/offices', officesRouter);
+
+module.exports = router;
