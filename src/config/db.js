0fb2922c3d18f3ab9e424a7b8fb96e44e79d70d4
JulioMourajr <juliomourajr@hotmail.com>
2023-05-30 22:42:53 -0300

    Mudando as configurações no db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,17 @@
-
 const sequelize = require('./sequelize')
+
+// Models
+
+const officesModel = require('./officesModel');
+const employeesModel = require('./employeesModel');
+
+// Relatioships
+
+officesModel.hasMany(employeesModel, {foreignKey: 'officeCode', onDelete: 'NO ACTION', onUpdate: 'CASCADE'});
+
+employeesModel.belongsTo(officesModel, {foreignKey: 'officeCode'});
+
+
 
 sequelize.sync({alter: process.env.NODE_ENV === 'production' ? false : true});
 
