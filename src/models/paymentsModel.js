fa90d121985bc074457d4359a8688033ebda71f3
JulioMourajr <juliomourajr@hotmail.com>
2023-06-05 22:15:35 -0300

    Criação do modelo da tabela de payments

---
+++ src/models/paymentsModel.js
@@ -0,0 +1,30 @@
+const sequelize = require('../config/sequelize');
+
+const { DataTypes } = require('sequelize');
+
+const paymentsModel = sequelize.define('payments', {
+  customerNumber: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+
+  },
+  checkNumber: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    primaryKey: true,
+  },
+  paymentDate:{
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+  amount: {
+    type: DataTypes.DECIMAL(10,2),
+    allowNull: false,
+  }
+}, {
+  tableName: 'payments',
+  timestamps: false
+});
+
+module.exports = paymentsModel;
